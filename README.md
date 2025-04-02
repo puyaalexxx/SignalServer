f34e8b1cf0de8f3ce61aabd06e787de43e573228
Alex <puyaalexxx@gmail.com>
2025-04-02 12:03:48 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# SignalServer
+# SignalServer - A SignalR - Pet Project
+
+<h2>Technologies used:</h2>
+
+1. Asp.Net.core
+2. SignalR
+
+<h2>Features:</h2>
+
+1. test
+
+<h2>Demo:</h2>
+
+![Demo 1]()
--- SignalServer/Hubs/LearningHub.cs
+++ SignalServer/Hubs/LearningHub.cs
@@ -1,6 +1,21 @@
+using Microsoft.AspNetCore.SignalR;
+
 namespace SignalServer.Hubs;
 
-public class LearningHub
+public class LearningHub : Hub
 {
-    
+    public async Task BroadcastMessage(string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", message);
+    }
+
+    public override async Task OnConnectedAsync()
+    {
+        await base.OnConnectedAsync();
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        await base.OnDisconnectedAsync(exception);
+    }
 }
