9a899bf766a211a31179a668bab3d86f0a3a1569
Alex <puyaalexxx@gmail.com>
2025-04-02 12:02:07 +0300

    Initial commit

---
+++ SignalServer/Hubs/LearningHub.cs
@@ -0,0 +1,6 @@
+namespace SignalServer.Hubs;
+
+public class LearningHub
+{
+    
+}
---
+++ SignalServer/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SignalServer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
